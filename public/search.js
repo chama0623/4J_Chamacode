d9a5a2eb95594eb396ebe02a4eb3601f3caa96ff
yudai0731 <yudai40018@gmail.com>
2020-12-15 09:15:00 +0900

    fine tuning

--- public/search.js
+++ public/search.js
@@ -2,10 +2,12 @@ var xhr = new XMLHttpRequest();
 
 function doSearch(){
     t = document.getElementById("searchform").value;
-    xhr.onreadystatechange = checkStatus;
-    xhr.open('GET','http://127.0.0.1:9998/search/'+t,true);
-    xhr.responseType = 'json';
-    xhr.send(null);
+    if(t.length!=0){
+        xhr.onreadystatechange = checkStatus;
+        xhr.open('GET','http://127.0.0.1:9998/search/'+t,true);
+        xhr.responseType = 'json';
+        xhr.send(null);
+    }
 }
 
 function checkStatus(){
@@ -13,18 +15,18 @@ function checkStatus(){
     if((xhr.readyState==4)&&(xhr.status==200)){
         a=xhr.response;
         l=a[0].kensu;
+        s="<div class=\"row mx-2\">"
         if(l==0){
-            s="見つかりませんでした.";
+            s=s+"見つかりませんでした.</div>";
         }else{
-            s=s+l+"件見つかりました.<br>";
+            s=s+l+"件見つかりました.</div><br>";
             if(l<=100){
                 s=s+"<table class=\"table table-bordered\">";
                 s=s+"<tr>";
-                s=s+"<th>title</th>";
-                s=s+"<th>username</th>";
+                s=s+"<th>タイトル</th>";
+                s=s+"<th>投稿者</th>";
                 s=s+"<th>更新日時</th>";
                 s=s+"<th>いいね数</th>";
-                s=s+"<th> </th>";
                 s=s+"<tr>";
                 for(i=1;i<=l;i++){
                     s=s+"<tr>";
@@ -38,7 +40,7 @@ function checkStatus(){
                     s=s+"<td>"+a[i].good+"</td>";
                     s=s+"<td>";
                     s=s+"<form method=\"post\" action=\"detail\">";
-                    s=s+"<input type=\"submit\" value=\"Detail\">";
+                    s=s+"<button type=\"submit\" class=\"btn btn-primary\">詳細</button>"
                     s=s+"<input type=\"hidden\" name=\"id\" value=\""+a[i].id+"\">";
                     s=s+"</form>";
                     s=s+"</td>";
